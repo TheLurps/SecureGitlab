63b28a46d5e7bf2349a46cb3f103fffe53608cd4
Joshua Schraven <joshua@schraven.email>
2022-04-18 17:23:29 +0200

    Depromote users from admin role

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SecureGitlab
+
 Secure GitLab instance using its REST API
+
+## Usage
+
+- generate private token with `api` permissions
+- call tool with:
+
+```bash
+```bash
+./secure_gitlab.py --url http://localhost --private-token 7Ha5jBvMP5G3fsa_ZSLD \
+    --disable-public-signup \
+    --depromote-users \
+    --except-user root
+```
--- secure_gitlab.py
+++ secure_gitlab.py
@@ -17,6 +17,16 @@ def disable_public_signup(url, token):
     else:
         print('Disabling public user signup failed')
 
+def fetch_users(url, token):
+    endpoint = '/api/v4/users'
+    r = requests.get(url + endpoint, headers={'PRIVATE-TOKEN': token})
+    return r.json()
+
+def depromote_user(url, token, userid):
+    endpoint = '/api/v4/users/' + str(userid)
+    r = requests.put(url + endpoint, headers={'PRIVATE-TOKEN': token}, data={'id': userid, 'admin': False})
+    print('Depromoting user {0}'.format(r.json()['username']))
+
 def main():
     parser=argparse.ArgumentParser()
 
@@ -24,6 +34,8 @@ def main():
     parser.add_argument('--private-token', dest='token', help='Private token of admin user', type=str)
 
     parser.add_argument('--disable-public-signup', dest='disable_public_signup', action='store_true', help='Disable public user signup')
+    parser.add_argument('--depromote-users', dest='depromote_users', action='store_true', help='Depromote users from admin role')
+    parser.add_argument('--except-user', dest='except_users', help='List of users that should should be excepted (comma-separated)')
 
     args=parser.parse_args()
 
@@ -34,6 +46,15 @@ def main():
     if args.disable_public_signup:
         disable_public_signup(args.url, args.token)
 
+    except_users = []
+    if args.except_users != None:
+        except_users = args.except_users.split(',')
+
+    if args.depromote_users:
+        users = fetch_users(args.url, args.token)
+        for user in users:
+            if user['username'] not in except_users:
+                depromote_user(args.url, args.token, user['id'])
 
 if __name__ == '__main__':
     main()
